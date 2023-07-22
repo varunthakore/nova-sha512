6f7b5d30b72218db9431a3e6a7f1bf289e7e6716
Varun Thakore <vrnthakore@gmail.com>
2023-07-22 15:14:48 +0530

    Update to latest versions - Update versions of ff, bellperson and nova-snark - Remove unwanted dependencies - cargo fmt

--- src/circuit.rs
+++ src/circuit.rs
@@ -1,14 +1,15 @@
+use super::util::{
+    digest_to_scalars, sha512_state_sequence, BLOCK_LENGTH_BYTES, DIGEST_LENGTH_BYTES,
+};
 use bellperson::{
-    gadgets::{num::AllocatedNum, multipack::pack_bits, boolean::AllocatedBit},
-    gadgets::{multipack::bytes_to_bits, boolean::Boolean},
+    gadgets::{boolean::AllocatedBit, multipack::pack_bits, num::AllocatedNum},
+    gadgets::{boolean::Boolean, multipack::bytes_to_bits},
     ConstraintSystem, SynthesisError,
 };
-use nova_snark::traits::circuit::StepCircuit;
-use pasta_curves::group::ff::{PrimeField, PrimeFieldBits};
 use bellperson_sha512::sha512::sha512_compression_function;
 use bellperson_sha512::uint64::UInt64;
-use super::util::{sha512_state_sequence, BLOCK_LENGTH_BYTES, DIGEST_LENGTH_BYTES, digest_to_scalars};
-
+use nova_snark::traits::circuit::StepCircuit;
+use pasta_curves::group::ff::{PrimeField, PrimeFieldBits};
 
 #[derive(Clone, Debug)]
 pub struct SHA512CompressionCircuit<F: PrimeField> {
@@ -17,14 +18,15 @@ pub struct SHA512CompressionCircuit<F: PrimeField> {
     next_digest: [F; 3],
 }
 
-impl<F> Default for SHA512CompressionCircuit<F> 
-where F: PrimeField + PrimeFieldBits,
+impl<F> Default for SHA512CompressionCircuit<F>
+where
+    F: PrimeField + PrimeFieldBits,
 {
     fn default() -> Self {
         Self {
             input: [0u8; BLOCK_LENGTH_BYTES],
-            current_digest: [F::zero(); 3],
-            next_digest: [F::zero(); 3],
+            current_digest: [F::ZERO; 3],
+            next_digest: [F::ZERO; 3],
         }
     }
 }
@@ -36,20 +38,16 @@ impl<F: PrimeField + PrimeFieldBits> SHA512CompressionCircuit<F> {
         let mut iteration_vec: Vec<SHA512CompressionCircuit<F>> = vec![];
 
         for i in 0..block_seq.len() {
-            iteration_vec.push(
-                SHA512CompressionCircuit {
-                    input: block_seq[i],
-                    current_digest: digest_to_scalars(&digest_seq[i]),
-                    next_digest: digest_to_scalars(&digest_seq[i+1]),
-                }
-            );
+            iteration_vec.push(SHA512CompressionCircuit {
+                input: block_seq[i],
+                current_digest: digest_to_scalars(&digest_seq[i]),
+                next_digest: digest_to_scalars(&digest_seq[i + 1]),
+            });
         }
 
         iteration_vec
     }
-
 }
-
 
 impl<F> StepCircuit<F> for SHA512CompressionCircuit<F>
 where
@@ -63,7 +61,7 @@ where
         &self,
         cs: &mut CS,
         z: &[AllocatedNum<F>],
-      ) -> Result<Vec<AllocatedNum<F>>, SynthesisError> {
+    ) -> Result<Vec<AllocatedNum<F>>, SynthesisError> {
         assert_eq!(z.len(), 3);
         let initial_curr_digest_bits = z[0]
             .to_bits_le(cs.namespace(|| "initial current digest bits"))
@@ -82,7 +80,7 @@ where
         for i in 0..F::CAPACITY as usize {
             current_digest_bits.push(next_curr_digest_bits[i].clone());
         }
-        let num_bits_remaining = DIGEST_LENGTH_BYTES*8 - (F::CAPACITY as usize)*2;
+        let num_bits_remaining = DIGEST_LENGTH_BYTES * 8 - (F::CAPACITY as usize) * 2;
         for i in 0..num_bits_remaining {
             current_digest_bits.push(remaining_curr_digest_bits[i].clone());
         }
@@ -92,26 +90,23 @@ where
             current_state.push(UInt64::from_bits_be(c));
         }
         assert_eq!(current_state.len(), 8);
-        
+
         let input_bit_values = bytes_to_bits(&self.input);
-        assert_eq!(input_bit_values.len(), BLOCK_LENGTH_BYTES*8);
+        assert_eq!(input_bit_values.len(), BLOCK_LENGTH_BYTES * 8);
         let input_bits: Vec<Boolean> = input_bit_values
             .iter()
             .enumerate()
-            .map(
-                |(i, b)| Boolean::from(
-                AllocatedBit::alloc(
-                    cs.namespace(|| format!("input bit {i}")),
-                    Some(*b),
-
-                    )
-                    .unwrap()
+            .map(|(i, b)| {
+                Boolean::from(
+                    AllocatedBit::alloc(cs.namespace(|| format!("input bit {i}")), Some(*b))
+                        .unwrap(),
                 )
-            )
+            })
             .collect();
 
         // SHA512 compression function application
-        let next_state: Vec<UInt64> = sha512_compression_function(&mut *cs, &input_bits, &current_state)?;
+        let next_state: Vec<UInt64> =
+            sha512_compression_function(&mut *cs, &input_bits, &current_state)?;
         assert_eq!(next_state.len(), 8);
 
         let next_digest_bits: Vec<Boolean> = next_state
@@ -119,29 +114,35 @@ where
             .map(|u| u.into_bits_be())
             .flatten()
             .collect();
-        assert_eq!(next_digest_bits.len(), DIGEST_LENGTH_BYTES*8);
+        assert_eq!(next_digest_bits.len(), DIGEST_LENGTH_BYTES * 8);
 
         let mut z_out: Vec<AllocatedNum<F>> = vec![];
-        let (initial_next_digest_bits, temp_next_digest_bits)
-            = next_digest_bits.split_at(F::CAPACITY as usize);
-        let (next_next_digest_bits, remaining_next_digest_bits)
-            = temp_next_digest_bits.split_at(F::CAPACITY as usize);
-        z_out.push(pack_bits(
-            cs.namespace(|| "Packing initial preimage bits into scalar"),
-            initial_next_digest_bits
-        )
-        .unwrap());
-        z_out.push(pack_bits(
-            cs.namespace(|| "Packing next preimage bits into scalar"),
-            next_next_digest_bits
-        )
-        .unwrap());
-        z_out.push(pack_bits(
-            cs.namespace(|| "Packing remaining preimage bits into scalar"),
-            remaining_next_digest_bits
-        )
-        .unwrap());
-        
+        let (initial_next_digest_bits, temp_next_digest_bits) =
+            next_digest_bits.split_at(F::CAPACITY as usize);
+        let (next_next_digest_bits, remaining_next_digest_bits) =
+            temp_next_digest_bits.split_at(F::CAPACITY as usize);
+        z_out.push(
+            pack_bits(
+                cs.namespace(|| "Packing initial preimage bits into scalar"),
+                initial_next_digest_bits,
+            )
+            .unwrap(),
+        );
+        z_out.push(
+            pack_bits(
+                cs.namespace(|| "Packing next preimage bits into scalar"),
+                next_next_digest_bits,
+            )
+            .unwrap(),
+        );
+        z_out.push(
+            pack_bits(
+                cs.namespace(|| "Packing remaining preimage bits into scalar"),
+                remaining_next_digest_bits,
+            )
+            .unwrap(),
+        );
+
         Ok(z_out)
     }
 
@@ -154,9 +155,7 @@ where
         // Compute output using non-deteriministic advice
         self.next_digest.to_vec()
     }
-
 }
-
 
 #[cfg(test)]
 mod tests {
@@ -170,25 +169,23 @@ mod tests {
         let empty_input: Vec<u8> = vec![];
         let mut sha512_state_sequence = SHA512CompressionCircuit::new_state_sequence(empty_input);
         assert_eq!(sha512_state_sequence.len(), 1);
-        
+
         let sha512_iteration = sha512_state_sequence.pop().unwrap();
         let mut z_in: Vec<AllocatedNum<Fp>> = vec![];
 
         for (i, s) in sha512_iteration.current_digest.iter().enumerate() {
-            z_in.push(AllocatedNum::alloc(
-                cs.namespace(|| format!("z_in[{i}]")),
-                || Ok(*s)
-            ).unwrap());
+            z_in.push(
+                AllocatedNum::alloc(cs.namespace(|| format!("z_in[{i}]")), || Ok(*s)).unwrap(),
+            );
         }
         let z_out = sha512_iteration.synthesize(&mut cs, &z_in).unwrap();
         assert!(cs.is_satisfied());
-        
+
         assert_eq!(z_out.len(), sha512_iteration.next_digest.len());
         for (i, s) in sha512_iteration.next_digest.iter().enumerate() {
             assert_eq!(z_out[i].get_value().unwrap(), *s);
         }
         println!("Num constraints = {:?}", cs.num_constraints());
         println!("Num inputs = {:?}", cs.num_inputs());
-
-    }    
+    }
 }
--- src/util.rs
+++ src/util.rs
@@ -1,22 +1,23 @@
+use bellperson::gadgets::multipack::{bytes_to_bits, compute_multipacking};
 use generic_array::{typenum::U128, GenericArray};
 use pasta_curves::group::ff::{PrimeField, PrimeFieldBits};
-use bellperson::gadgets::multipack::{compute_multipacking, bytes_to_bits};
 use sha2::compress512;
 
-
-
 pub const IV: [u64; 8] = [
-    0x6a09e667f3bcc908, 0xbb67ae8584caa73b, 0x3c6ef372fe94f82b, 0xa54ff53a5f1d36f1,
-    0x510e527fade682d1, 0x9b05688c2b3e6c1f, 0x1f83d9abfb41bd6b, 0x5be0cd19137e2179,
+    0x6a09e667f3bcc908,
+    0xbb67ae8584caa73b,
+    0x3c6ef372fe94f82b,
+    0xa54ff53a5f1d36f1,
+    0x510e527fade682d1,
+    0x9b05688c2b3e6c1f,
+    0x1f83d9abfb41bd6b,
+    0x5be0cd19137e2179,
 ];
 
 pub const BLOCK_LENGTH_BYTES: usize = 128;
 pub const DIGEST_LENGTH_BYTES: usize = 64;
 
-
-pub fn sha512_state_to_bytes(
-    state: [u64; 8],
-) -> Vec<u8> {
+pub fn sha512_state_to_bytes(state: [u64; 8]) -> Vec<u8> {
     state
         .into_iter()
         .map(|x| x.to_be_bytes())
@@ -24,10 +25,7 @@ pub fn sha512_state_to_bytes(
         .collect()
 }
 
-
-fn padded_input_to_blocks(
-    input: Vec<u8>,
-) -> Vec<GenericArray<u8, U128>> {
+fn padded_input_to_blocks(input: Vec<u8>) -> Vec<GenericArray<u8, U128>> {
     assert!(input.len() % BLOCK_LENGTH_BYTES == 0);
 
     let blocks = input.chunks(BLOCK_LENGTH_BYTES);
@@ -38,30 +36,29 @@ fn padded_input_to_blocks(
     blocks_vec
 }
 
-
-fn add_sha512_padding(
-    input: Vec<u8>,
-) -> Vec<u8> {
-    let length_in_bits = (input.len()*8) as u128;
+fn add_sha512_padding(input: Vec<u8>) -> Vec<u8> {
+    let length_in_bits = (input.len() * 8) as u128;
     let mut padded_input = input;
 
-    // appending a single '1' bit followed by 7 '0' bits 
+    // appending a single '1' bit followed by 7 '0' bits
     // This is because the input is a byte vector
     padded_input.push(128u8);
 
     // Append zeros until the padded input (including 16-byte length)
     // is a multiple of 128 bytes. Note that input is always a byte vector.
-    while (padded_input.len() + 16) % BLOCK_LENGTH_BYTES !=0 {
+    while (padded_input.len() + 16) % BLOCK_LENGTH_BYTES != 0 {
         padded_input.push(0u8);
     }
     padded_input.append(&mut length_in_bits.to_be_bytes().to_vec());
     padded_input
 }
 
-
 pub fn sha512_state_sequence(
     input: Vec<u8>,
-) -> (Vec<[u8; BLOCK_LENGTH_BYTES]>, Vec<[u8; DIGEST_LENGTH_BYTES]>) {
+) -> (
+    Vec<[u8; BLOCK_LENGTH_BYTES]>,
+    Vec<[u8; DIGEST_LENGTH_BYTES]>,
+) {
     let padded_input = add_sha512_padding(input);
 
     let mut state = IV;
@@ -82,35 +79,53 @@ pub fn sha512_state_sequence(
     (block_sequence, digest_sequence)
 }
 
-
 pub fn digest_to_scalars<F>(digest: &[u8; DIGEST_LENGTH_BYTES]) -> [F; 3]
-where F: PrimeField + PrimeFieldBits
+where
+    F: PrimeField + PrimeFieldBits,
 {
-    compute_multipacking(&bytes_to_bits(digest)).try_into().unwrap()
+    compute_multipacking(&bytes_to_bits(digest))
+        .try_into()
+        .unwrap()
 }
 
-
 pub fn sha512_initial_digest_scalars<F>() -> Vec<F>
-where F: PrimeField + PrimeFieldBits
+where
+    F: PrimeField + PrimeFieldBits,
 {
-    let initial_vector: [u8; DIGEST_LENGTH_BYTES] = sha512_state_to_bytes(IV).as_slice().try_into().unwrap();
+    let initial_vector: [u8; DIGEST_LENGTH_BYTES] =
+        sha512_state_to_bytes(IV).as_slice().try_into().unwrap();
     digest_to_scalars(&initial_vector).to_vec()
 }
 
-
 pub fn scalars_to_digest<F>(scalars: [F; 3]) -> [u8; DIGEST_LENGTH_BYTES]
-where F: PrimeField + PrimeFieldBits
+where
+    F: PrimeField + PrimeFieldBits,
 {
     let mut digest_bits: Vec<bool> = vec![];
     let initial_bits = scalars[0].to_le_bits();
-    digest_bits.append(&mut initial_bits.into_iter().take(F::CAPACITY as usize).collect());
+    digest_bits.append(
+        &mut initial_bits
+            .into_iter()
+            .take(F::CAPACITY as usize)
+            .collect(),
+    );
 
     let next_initial_bits = scalars[1].to_le_bits();
-    digest_bits.append(&mut next_initial_bits.into_iter().take(F::CAPACITY as usize).collect());
+    digest_bits.append(
+        &mut next_initial_bits
+            .into_iter()
+            .take(F::CAPACITY as usize)
+            .collect(),
+    );
 
     let remaining_bits = scalars[2].to_le_bits();
-    let num_bits_remaining = DIGEST_LENGTH_BYTES*8 - (F::CAPACITY as usize)*2;
-    digest_bits.append(&mut remaining_bits.into_iter().take(num_bits_remaining).collect());
+    let num_bits_remaining = DIGEST_LENGTH_BYTES * 8 - (F::CAPACITY as usize) * 2;
+    digest_bits.append(
+        &mut remaining_bits
+            .into_iter()
+            .take(num_bits_remaining)
+            .collect(),
+    );
 
     assert_eq!(digest_bits.len() % 8, 0);
     assert_eq!(digest_bits.len() / 8, DIGEST_LENGTH_BYTES);
@@ -121,7 +136,7 @@ where F: PrimeField + PrimeFieldBits
         let mut coeff = 1u8;
         for j in 0..8usize {
             // The digest bits are interpreted as big-endian bytes
-            if digest_bits[8*i + 7 - j] {
+            if digest_bits[8 * i + 7 - j] {
                 byte_val += coeff
             }
             coeff <<= 1;
@@ -131,12 +146,11 @@ where F: PrimeField + PrimeFieldBits
     digest.as_slice().try_into().unwrap()
 }
 
-
 #[cfg(test)]
 mod test {
-    use pasta_curves::Fp;
-    use hex;
     use super::*;
+    use hex;
+    use pasta_curves::Fp;
 
     #[test]
     fn test_one_compression_iteration() {
@@ -187,16 +201,14 @@ mod test {
         }
     }
 
-
     #[test]
     fn test_scalar_digest_roundtrip() {
         let initial_scalars: Vec<Fp> = sha512_initial_digest_scalars();
         let computed_bytes = scalars_to_digest(initial_scalars.clone().try_into().unwrap());
         let expected_bytes = sha512_state_to_bytes(IV);
-        assert_eq!(expected_bytes.len(), computed_bytes.len()); 
+        assert_eq!(expected_bytes.len(), computed_bytes.len());
         for i in 0..computed_bytes.len() {
             assert_eq!(expected_bytes[i], computed_bytes[i]);
         }
-    } 
-
+    }
 }
